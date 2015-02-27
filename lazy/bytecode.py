81f53fbcfea4d8cdd2a30239a50bac258334baa3
llllllllll <joe@quantopian.com>
2015-02-27 03:02:28 -0500

    DEV: Starts working on the bytecode transformer

--- lazy/bytecode.py
+++ lazy/bytecode.py
@@ -1,284 +1,315 @@
-from dis import Bytecode, opmap, hasjabs
+from abc import ABCMeta, abstractproperty
+from dis import Bytecode, opname, opmap, hasjabs, hasjrel
 from operator import is_, not_
 from types import CodeType, FunctionType
 
-from lazy._thunk import thunk, strict
-from lazy.utils import isolate_namespace
+from lazy._thunk import strict, thunk
 
 
-def _getcode(func_or_code):
-    if isinstance(func_or_code, CodeType):
-        return func_or_code
-    else:
-        return func_or_code.__code__
+def _sparse_args(instrs):
+    """
+    Makes the arguments sparse so that instructions live at the correct
+    index for the jump resolution step.
+    The `None` instructions will be filtered out.
+    """
+    for instr in instrs:
+        yield instr
+        if instr.arg is not None:
+            yield None
+            yield None
 
 
-class Opmap(dict):
+class ops(dict):
     def __getattr__(self, name):
-        return bytes((self[name],))
+        return self[name]
 
-ops = Opmap(opmap)
-
-
-def thunkify(tuple_, *, _thunk_=thunk):
-    for t in tuple_ or ():
-        yield _thunk_.fromvalue(t)
+ops = ops(opmap)
 
 
-def _const_convert(c, _globals=None):
-    if isinstance(c, CodeType):
-        return LazyConverter(c, _globals).converted_code
-    else:
-        return thunk.fromvalue(c)
-
-
-def _lazy_is(a, b, *, is_=is_):
+class CodeTransformer(object, metaclass=ABCMeta):
     """
-    Used instead of the `is` operator.
+    A code object transformer, simmilar to the AstTransformer from the ast
+    module.
     """
-    return thunk(is_, a, b)
-
-
-def _lazy_not(a, *, not_=not_):
-    """
-    Used instead of the `not` operator.
-    """
-    return thunk(not_, a)
-
-
-class LazyConverter(object):
-    class Consts(object):
-        def __init__(self):
-            self._idx = 0
-            self._obs = {}
-
-        def index(self, ob):
-            try:
-                return self._obs[ob]
-            except KeyError:
-                self._obs[ob] = idx = self._idx.to_bytes(2, 'little')
-                self._idx += 1
-                return idx
-
-        @property
-        def contents(self):
-            obs = self._obs
-            return tuple(sorted(obs, key=lambda e: obs[e]))
-
-    class LazyTransformations(dict):
-        def __new__(cls, converter):
-            return super().__new__(cls)
-
-        def __init__(self, converter):
-            self._converter = converter
-
-        def __getitem__(self, opname):
-            return getattr(
-                self._converter,
-                'transform_' + opname,
-                self._converter._default,
-            )
-
-        def __getattr__(self, opname):
-            return self[opname]
-
-    def __init__(self, f, _globals=None):
-        self.f = f
-        self.code = _getcode(f)
-
-        # Construct the const objects.
-        self._consts = self.Consts()
-        for c in self.code.co_consts + (strict, thunk.fromvalue):
-            self._consts.index(c)
-
-        self.transformations = self.LazyTransformations(self)
-        self._globals = _globals if _globals is not None else f.__globals__
-        self._call_args_idx = None
-        self._call_kwargs_idx = None
-        self._co_total_argcount = None
-        self._jmp_targets = {}
-
-    def _default(self, opcode, arg):
+    @abstractproperty
+    def stack_modifier(self):
         """
-        The default opcode behavior.
+        How much does this transformer affect the maximum stack usage.
         """
-        yield bytes((opcode,))
-        if arg is not None:
-            yield arg.to_bytes(2, 'little')
+        return 0
 
-    @property
-    def converted_function(self):
-        f = self.f
+    def __init__(self):
+        self._instrs = None
+        self._consts = None
 
-        return FunctionType(
-            self.converted_code,
-            f.__globals__,
-            f.__name__,
-            tuple(thunkify(f.__defaults__)),
-            f.__closure__,
+    def __getitem__(self, idx):
+        return self._instrs[idx]
+
+    def index(self, instr):
+        """
+        Returns the index of an `Instruction`.
+        """
+        return self._instrs.index(instr)
+
+    def __iter__(self):
+        return iter(self._instrs)
+
+    def const_index(self, obj):
+        """
+        The index of a constant.
+        If `obj` is not already a constant, it will be added to the consts
+        and given a new const index.
+        """
+        try:
+            return self._consts[obj]
+        except KeyError:
+            self._consts[obj] = ret = self._const_idx
+            self._const_idx += 1
+            return ret
+
+    def visit_generic(self, instr):
+        if instr is None:
+            return
+
+        yield from getattr(self, 'visit_' + instr.opname, lambda *a: a)(instr)
+
+    def visit_const(self, const):
+        """
+        Override this method to transform the `co_consts` of the code object.
+        """
+        if isinstance(const, CodeType):
+            return type(self).visit(const)
+        else:
+            return const
+
+    def _id(self, obj):
+        """
+        Identity function.
+        """
+        return obj
+
+    visit_name = _id
+    visit_varname = _id
+    visit_freevar = _id
+    visit_cellvar = _id
+    visit_default = _id
+
+    del _id
+
+    def visit(self, co, name=None):
+        """
+        Visit a code object, applying the transforms.
+        """
+        # WARNING:
+        # This is setup in this double assignment way because jump args
+        # must backreference their original jump target before any transforms.
+        # Don't refactor this into a single pass.
+        self._instrs = list(_sparse_args([
+            Instruction(b.opcode, b.arg) for b in Bytecode(co)
+        ]))
+        self._instrs = [
+            instr and instr._with_jmp_arg(self) for instr in self._instrs
+        ]
+        self._consts = {
+            self.visit_const(k): idx for idx, k in enumerate(co.co_consts)
+        }
+        self._const_idx = len(co.co_consts)
+
+        self._instrs = sum(
+            (tuple(self.visit_generic(_instr)) for _instr in self),
+            (),
         )
-
-    @property
-    def converted_code(self):
-        """
-        Constructs a lazy code object.
-        """
-        co = self.code
-
-        co_varnames = co.co_varnames + (
-            isolate_namespace('_call_var'),
-            isolate_namespace('_kwargs_var'),
-        )
-
-        len_co_varnames = len(co_varnames)
-        self._call_kwargs_idx = (len_co_varnames - 1).to_bytes(2, 'little')
-        self._call_args_idx = (len_co_varnames - 2).to_bytes(2, 'little')
-        self._co_total_argcount = co.co_argcount + co.co_kwonlyargcount
-
-        bc = b''.join(self._fix_abs_offsets(self._lazy_bytecode))
 
         return CodeType(
             co.co_argcount,
             co.co_kwonlyargcount,
             co.co_nlocals,
-            co.co_stacksize + 1,
+            co.co_stacksize + self.stack_modifier,
             co.co_flags,
-            bc,
-            self._consts.contents,
-            co.co_names,
-            co_varnames,
+            b''.join(instr.to_bytecode(self) for instr in self),
+            tuple(sorted(self._consts, key=lambda c: self._consts[c])),
+            tuple(self.visit_name(n) for n in co.co_names),
+            tuple(self.visit_varname(n) for n in co.co_varnames),
             co.co_filename,
-            co.co_name,
+            name if name is not None else co.co_name,
             co.co_firstlineno,
             co.co_lnotab,
-            co.co_freevars,
-            co.co_cellvars,
+            tuple(self.visit_freevar(c) for c in co.co_freevars),
+            tuple(self.visit_cellvar(c) for c in co.co_cellvars),
         )
 
-    def _fix_abs_offsets(self, bc):
-        it = iter(tuple(bc))
-        for b in it:
-            yield b
+    def __repr__(self):
+        return '<{cls}: {instrs!r}>'.format(
+            cls=type(self).__name__,
+            instrs=self._instrs,
+        )
 
-            if int.from_bytes(b, 'little') in hasjabs:
-                offset = next(it)
-                yield self._jmp_targets[int.from_bytes(offset, 'little')]
+
+class Instruction(object):
+    """
+    An abstraction of an instruction.
+    This must live in a code object.
+    """
+    def __init__(self, opcode, arg=None):
+        self.opcode = opcode
+        self.arg = arg
+        self.reljmp = False
+        self.absjmp = False
+
+    def _with_jmp_arg(self, transformer):
+        """
+        If this is a jump opcode, then convert the arg to the instruction
+        to jump to.
+        """
+        opcode = self.opcode
+        if opcode in hasjrel:
+            self.arg = transformer[self.index(transformer) + 2 + self.arg]
+            self.reljmp = True
+        elif opcode in hasjabs:
+            self.arg = transformer[self.arg]
+            self.absjmp = True
+        return self
 
     @property
-    def _lazy_bytecode(self):
-        """
-        Applies the lazy bytecode transformations.
-        """
-        new_idx = 0
-        for b in Bytecode(self.code):
-            if b.is_jump_target:
-                self._jmp_targets[b.offset] = new_idx.to_bytes(2, 'little')
+    def opname(self):
+        return opname[self.opcode]
 
-            for t in self.transformations[b.opname](b.opcode, b.arg):
-                new_idx += len(t)
-                yield t
+    def to_bytecode(self, transformer):
+        bs = bytes((self.opcode,))
+        arg = self.arg
+        if isinstance(arg, Instruction):
+            if self.absjmp:
+                bs += arg.index(transformer).to_bytes(2, 'little')
+            elif self.reljmp:
+                bs += (
+                    arg.index(transformer) - self.index(transformer)
+                ).to_bytes(2, 'little')
+            else:
+                raise ValueError('must be relative or absolute jump')
+        elif arg is not None:
+            bs += arg.to_bytes(2, 'little')
+        return bs
 
-    def transform_MAKE_FUNCTION(self, opcode, arg):
+    def index(self, transformer):
+        return transformer.index(self)
+
+    def __repr__(self):
+        arg = self.arg
+        return '<{cls}: {opname}{arg}>'.format(
+            cls=type(self).__name__,
+            opname=self.opname,
+            arg=': ' + str(arg) if self.arg is not None else '',
+        )
+
+
+def _lazy_is(a, b, *, is_=is_):
+    return thunk(is_, a, b)
+
+
+def _lazy_not(a, *, not_=not_):
+    return thunk(not_, a)
+
+
+class LazyTransformer(CodeTransformer):
+    @property
+    def stack_modifier(self):
+        return 1
+
+    def visit_CONST(self, const):
+        const = super().visit_CONST(const)
+        if not isinstance(const, CodeType):
+            const = thunk.fromvalue(const)
+        return const
+
+    visit_freevar = thunk.fromvalue
+    visit_cellvar = thunk.fromvalue
+
+    def visit_MAKE_FUNCTION(self, instr):
         """
         Functions should have strict names.
         """
-        yield ops.LOAD_CONST
-        yield self._consts.index(strict)
-        # TOS  = strict
+        yield Instruction(ops.LOAD_CONST, self.const_index(strict))
+        # TOS = strict
         # TOS1 = func_name
 
-        yield ops.ROT_TWO
-        # TOS  = func_name
+        yield Instruction(ops.ROT_TWO)
+        # TOS = func_name
         # TOS1 = strict
 
-        yield ops.CALL_FUNCTION
-        yield b'\x01\x00'
-        # TOS  = strict(func_name)
+        yield Instruction(ops.CALL_FUNCTION, 1)
+        # TOS = strict(func_name)
 
-        yield bytes((opcode,))
-        yield arg.to_bytes(2, 'little')
-        # TOS  = new_function
+        yield instr
+        # TOS = new_function
 
-    transform_MAKE_CLOSURE = transform_MAKE_FUNCTION
+    visit_MAKE_CLOSURE = visit_MAKE_FUNCTION
 
-    def _transform_name(self, opcode, arg):
+    def _visit_load_name(self, instr):
         """
         Loading a name immediatly wraps it in a `thunk`.
         """
-        yield ops.LOAD_CONST
-        yield self._consts.index(thunk.fromvalue)
-        # TOS  = thunk.fromvalue
+        yield Instruction(ops.LOAD_CONST, self.const_index(thunk.fromvalue))
+        # TOS = thunk.fromvalue
 
-        yield bytes((opcode,))
-        yield arg.to_bytes(2, 'little')
-        # TOS  = value
+        yield instr
+        # TOS = value
         # TOS1 = thunk.fromvalue
 
-        yield ops.CALL_FUNCTION
-        yield b'\x01\x00'
-        # TOS  = thunk.fromvalue(value)
+        yield Instruction(ops.CALL_FUNCTION, 1)
+        # TOS = thunk.fromvalue(value)
 
-    transform_LOAD_NAME = transform_LOAD_GLOBAL = _transform_name
+    visit_LOAD_NAME = visit_LOAD_GLOBAL = visit_LOAD_FAST = _visit_load_name
 
-    def transform_LOAD_FAST(self, opcode, arg):
-        """
-        Wrap arg lookups in thunks to be safe.
-        """
-        if arg > self._co_total_argcount:
-            yield from self._default(opcode, arg)
-        else:
-            yield from self._transform_name(opcode, arg)
-
-    def transform_COMPARE_OP(self, opcode, arg):
+    def visit_COMPARE_OP(self, instr):
         """
         Replace the `is` operator to act on the values the thunks represent.
         This makes `is` lazy.
         """
-        if arg != 8:  # is
-            yield from self._default(opcode, arg)
+        if instr.arg != 8: # is
+            yield from self.visit_generic(instr)
             return
 
-        yield ops.LOAD_CONST
-        yield self._consts.index(_lazy_is)
-        # TOS  = _lazy_is
+        yield Instruction(ops.LOAD_CONST, self.const_index(_lazy_is))
+        # TOS = _lazy_is
         # TOS1 = a
         # TOS2 = b
 
         # This safe to do because `is` is commutative 100% of the time.
         # We are doing a pointer compare so we can move the operands around.
         # This saves us from doing an extra ROT_TWO to preserve the order.
-        yield ops.ROT_THREE
-        # TOS  = a
+        yield Instruction(ops.ROT_THREE)
+        # TOS = a
         # TOS1 = b
         # TOS2 = _lazy_is
 
-        yield ops.CALL_FUNCTION
-        yield b'\x02\x00'
-        # TOS  = _lazy_is(b, a)
+        yield Instruction(ops.CALL_FUNCTION, 2)
+        # TOS = _lazy_is(b, a)
 
-    def transform_UNARY_NOT(self, opcode, arg):
+    def visit_UNARY_NOT(self, instr):
         """
         Replace the `not` operator to act on the values that the thunks
         represent.
         This makes `not` lazy.
         """
-        yield ops.LOAD_CONST
-        yield self._consts.index(_lazy_not)
-        # TOS  = _lazy_not
+        yield Instruction(ops.LOAD_CONST, self.const_index(_lazy_not))
+        # TOS = _lazy_not
         # TOS1 = arg
 
-        yield ops.ROT_TWO
-        # TOS  = arg
+        yield Instruction(ops.ROT_TWO)
+        # TOS = arg
         # TOS1 = _lazy_not
 
-        yield ops.CALL_FUNCTION
-        yield b'\x01\x00'
-        # TOS  = _lazy_not(arg)
+        yield Instruction(ops.CALL_FUNCTION, 1)
+        # TOS = _lazy_not(arg)
 
 
 def lazy_function(f):
-    """
-    Creates a function whose body is lazily evaluated.
-    Returns the function as a thunk.
-    """
-    return thunk.fromvalue(LazyConverter(f).converted_function)
+    return thunk.fromvalue(
+        FunctionType(
+            LazyTransformer().visit(f.__code__),
+            f.__globals__,
+            f.__name__,
+            tuple(map(thunk.fromvalue, f.__defaults__ or ())),
+            f.__closure__,
+        ),
+    )
